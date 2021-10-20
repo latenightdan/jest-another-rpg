3d98a42ba975eac3b4a7d1bac104bc223cb9b34e
daniel natale <dnatale667@gmail.com>
2021-10-20 13:53:19 -0400

    oearib

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,4 @@
-const { TestWatcher } = require('@jest/core');
+const { TestWatcher, createTestScheduler } = require('@jest/core');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
@@ -29,3 +29,30 @@ test('gets inventory from player or returns false', () =>{
 
     expect(player.getInventory()).toEqual(false);
 });
+
+test("gets player's health value", () =>{
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () =>{
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+test("subtracts from player's health", () =>{
+const player = new Player('Dave');
+const oldHealth = player.health;
+
+player.reduceHealth(5);
+
+expect(player.health).toBe(oldHealth - 5);
+
+player.reduceHealth(99999);
+expect(player.health).toBe(0);
+})
